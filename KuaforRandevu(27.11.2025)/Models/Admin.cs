6810fa232fa8807a8e7fb099d09d6ed89e248cc4
Fevzi Güler <fvz_glr@hotmail.com>
2025-11-27 14:53:08 +0300

    KuaforRandevu(27.11.2025)

---
+++ KuaforRandevu(27.11.2025)/Models/Admin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KuaforRandevu.Models;
+
+public partial class Admin
+{
+    public int Id { get; set; }
+
+    public string? Username { get; set; }
+
+    public string? Password { get; set; }
+
+    public byte[]? Photo { get; set; }
+
+    public virtual ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
+
+    public virtual ICollection<BlockedSlot> BlockedSlots { get; set; } = new List<BlockedSlot>();
+}
